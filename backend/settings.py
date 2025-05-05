8f3e18f37350a96669b34012f3927d8f97fa2a91
sdas171 <sdas1@miraclesoft.com>
2025-05-05 22:55:39 +0530

    aaaaaaaaa

--- backend/settings.py
+++ backend/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,10 +21,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-mu0au_ox$%z!0m$4u*$-_=$0#=5h!aycb^ax(7409ymvcxt5go'
+# SECRET_KEY = 'django-insecure-mu0au_ox$%z!0m$4u*$-_=$0#=5h!aycb^ax(7409ymvcxt5go'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+# DEBUG = True
+DEBUG = config('DEBUG', default=False, cast=bool)
+
 
 ALLOWED_HOSTS = []
 
@@ -40,11 +44,6 @@ INSTALLED_APPS = [
         'rest_framework',
     'api'
 ]
-INSTALLED_APPS += ['corsheaders']
-
-MIDDLEWARE.insert(0, 'corsheaders.middleware.CorsMiddleware')
-
-CORS_ALLOW_ALL_ORIGINS = True  # For dev only!
 
 
 MIDDLEWARE = [
