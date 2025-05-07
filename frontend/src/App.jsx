0bdd0670b5fc7a893722f480a5569370b4b6f5c1
sdas171 <sdas1@miraclesoft.com>
2025-05-07 21:26:34 +0530

    dev changes django

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,15 +1,11 @@
 import React from 'react'
-import Header from './pages/Header'
 import Main from './pages/Main'
-import Footer from './pages/Footer'
 
 function App() {
 
   return (
     <>
-    <Header/>
     <Main/>
-    <Footer/>
 
     </>
   )
--- frontend/src/pages/Main.jsx
+++ frontend/src/pages/Main.jsx
@@ -1,18 +1,26 @@
 import React from 'react';
 import Button from '../assets/resuable/Button';
+import Header from './Header';
+import Footer from './Footer'
 
 const Main = () => {
   return (
-    <div className="container">
-      <div className="p-5 text-center bg-dark rounded mt-5">
-        <h1 className="text-light">Stock Prediction Portal</h1>
-        <p className="lead text-light">
-          This stock prediction application utilizes machine learning techniques, specifically a Keras-based LSTM model, integrated within the Django framework. 
-          It forecasts future stock prices by analyzing 100-day and 200-day moving averages—key indicators widely used by analysts to support trading and investment decisions.
-        </p>
-        <Button  classample="btn btn-danger" text="Get Prediction"/>
-      </div>
-    </div>
+    <>
+     <Header/>
+<div className="container">
+  <div className="p-5 text-center bg-dark rounded mt-5">
+    <h1 className="text-light">Stock Prediction Portal</h1>
+    <p className="lead text-light">
+      This stock prediction application utilizes machine learning techniques, specifically a Keras-based LSTM model, integrated within the Django framework. 
+      It forecasts future stock prices by analyzing 100-day and 200-day moving averages—key indicators widely used by analysts to support trading and investment decisions.
+    </p>
+    <Button  classample="btn btn-danger" text="Get Prediction"/>
+  </div>
+</div>
+    <Footer/>
+    </>
+   
+
   );
 };
 
