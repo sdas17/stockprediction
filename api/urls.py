ee4407f97417f1077664dfcc679e0a007584c93c
sdas171 <sdas1@miraclesoft.com>
2025-05-13 00:06:44 +0530

    login functionalty

--- api/urls.py
+++ api/urls.py
@@ -1,10 +1,13 @@
 from django.urls import path
 from .views import UserCreateView
-from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
-
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
+from rest_framework_simplejwt.views import TokenVerifyView
 urlpatterns = [
     path("register/",UserCreateView.as_view()),
-     path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
-    
+     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+        path('api/token/verify/', TokenVerifyView.as_view(), name='token_verify'),
 ]
